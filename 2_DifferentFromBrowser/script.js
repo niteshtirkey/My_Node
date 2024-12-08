1bc5e82e59a402a19a9e1dbd4ee585890433a731
nitesh <niteshtirkey4@gmail.com>
2024-12-08 14:40:50 +0530

    different btw browser and server

---
+++ 2_DifferentFromBrowser/script.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+
+
+// const text = fs.readFileSync("//home//nitesh//Disk D//text.txt"); // out of local dirc
+// fs.writeFileSync("text.txt", "Your content here.., Hello")
+// fs.renameSync("text.txt", "Hello.txt") // file name change...
+// fs.unlinkSync("Hello.txt") // file remove
+
+// const text = fs.readFileSync("text.txt")
+
+// console.log(text.toString())
+// console.log(global)
+console.log("End")
